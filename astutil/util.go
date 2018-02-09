89178138bbfc5f356cf762f39e6c226c7a30ab0a
Alex Browne <stephenalexbrowne@gmail.com>
2018-02-08 19:03:46 -0800

    Add astutil package

---
+++ astutil/util.go
@@ -0,0 +1,16 @@
+package astutil
+
+import (
+	"github.com/albrow/fo/ast"
+)
+
+// Unparen returns e with any enclosing parentheses stripped.
+func Unparen(e ast.Expr) ast.Expr {
+	for {
+		p, ok := e.(*ast.ParenExpr)
+		if !ok {
+			return e
+		}
+		e = p.X
+	}
+}
