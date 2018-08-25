c2e3910c00450b8e161c08fa69f3d271376fe279
Alex Browne <stephenalexbrowne@gmail.com>
2018-08-25 16:19:47 -0700

    Support more primative types in transform/reflect.go

--- transform/reflect.go
+++ transform/reflect.go
@@ -202,7 +202,20 @@ func makePrimitiveZeroVal(typ *ast.Ident) ast.Expr {
 			Kind:  token.STRING,
 			Value: `""`,
 		}
-		// TODO(albrow): add support for remaining primitive types
+	case "bool":
+		return ast.NewIdent("false")
+	case "byte", "rune", "int", "int8", "int16", "int32", "int64",
+		"uint", "uint8", "uint16", "uint32", "uint64", "float32", "float64",
+		"complex64", "complex128", "uintptr":
+		return &ast.CallExpr{
+			Fun: ast.NewIdent(typ.String()),
+			Args: []ast.Expr{
+				&ast.BasicLit{
+					Kind:  token.INT,
+					Value: "0",
+				},
+			},
+		}
 	}
 	panic(fmt.Errorf("unknown primitive type: %T %s", typ, typ))
 }
